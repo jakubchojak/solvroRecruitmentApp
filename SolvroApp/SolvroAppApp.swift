b84dae187ccc59b4260cd262e80d8b0022e2f950
Jakub Chojak <abukkajohc@gmail.com>
2023-03-20 20:13:48 +0100

    Initial Commit

---
+++ SolvroApp/SolvroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolvroAppApp.swift
+//  SolvroApp
+//
+//  Created by Jakub Chojak on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SolvroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
